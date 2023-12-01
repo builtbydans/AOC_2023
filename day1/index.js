5a0d7d03ed2cdf79ea04d0b584ed65b8408189b8
builtbydans <danish.shafi@live.co.uk>
2023-12-01 13:56:19 +0000

    day 1 scaffold

---
+++ day1/index.js
@@ -0,0 +1,13 @@
+import { puzzle } from "./puzzle";
+
+// create a function that maps over puzzle data
+    // split each entry in the array
+    // condition to remove all letters and join numbers together
+    // return new array
+
+// map over new numbers array
+    // condition to only keep first and last digit in each item of the array
+    // return these two numbers
+
+// use reducer to sum all these numbers
+    // return final sum value
